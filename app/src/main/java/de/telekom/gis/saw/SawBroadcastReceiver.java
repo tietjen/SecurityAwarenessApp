112bb7246e221e916c16f10e52cf132bcde2f952
Jan Tietjen <jan.tietjen@parasec.de>
2014-03-22 15:45:06 +0100

    Initial Commit

---
+++ app/src/main/java/de/telekom/gis/saw/SawBroadcastReceiver.java
@@ -0,0 +1,20 @@
+package de.telekom.gis.saw;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class SawBroadcastReceiver extends BroadcastReceiver {
+	@Override
+    public void onReceive(Context context, Intent intent) {
+        if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+        /*
+         * start CheckerService once after system boot
+         * everything else is done inside CheckerService	
+         */
+        	Log.v("SawBroadcastReceiver", "BOOT_COMPLETED received...");
+        	context.startService(new Intent(context, CheckerService.class));
+        }
+    }
+}
